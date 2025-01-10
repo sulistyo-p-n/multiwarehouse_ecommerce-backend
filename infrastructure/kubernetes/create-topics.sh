159b86edbe797c75e305b9ef4e8c74b524cb54b6
UIOUIOUIO <sulistyo.p.n@gmail.com>
2025-01-10 13:17:24 +0700

    update kafka

--- infrastructure/kubernetes/create-topics.sh
+++ infrastructure/kubernetes/create-topics.sh
@@ -4,11 +4,11 @@
 kafka-topics --zookeeper $1:2181 --topic warehouse-change --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic product-change --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic user-change --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic inventory-product-stock-change --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic inventory-change --delete --if-exists
 
 kafka-topics --zookeeper $1:2181 --topic warehouse-change --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper $1:2181 --topic product-change --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper $1:2181 --topic user-change --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic inventory-product-stock-change --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic inventory-change --create --partitions 3 --replication-factor 3 --if-not-exists
 
 kafka-topics --zookeeper $1:2181 --list
